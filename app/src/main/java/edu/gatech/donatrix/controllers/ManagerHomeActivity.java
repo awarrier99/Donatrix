e0ac56a44b43ae7cea72925faeaa79b6bc9e2873
zday123 <zday123@yahoo.com>
2018-11-05 13:09:01 -0500

    Had to fix some STUPID shit Davidson did. Manifest should work now.

--- app/src/main/java/edu/gatech/donatrix/controllers/ManagerHomeActivity.java
+++ app/src/main/java/edu/gatech/donatrix/controllers/ManagerHomeActivity.java
@@ -1,4 +1,15 @@
 package edu.gatech.donatrix.controllers;
 
-public class ManagerHomeActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.donatrix.R;
+
+public class ManagerHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_home);
+    }
 }
