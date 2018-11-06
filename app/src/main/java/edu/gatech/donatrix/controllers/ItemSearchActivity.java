f3904901bc0460a55009e97ffb9461a0d8b5bc58
Ashvin Warrier <warrierar@gmail.com>
2018-11-06 14:30:16 -0500

    Stuff

--- app/src/main/java/edu/gatech/donatrix/controllers/ItemSearchActivity.java
+++ app/src/main/java/edu/gatech/donatrix/controllers/ItemSearchActivity.java
@@ -33,39 +33,45 @@ public class ItemSearchActivity extends AppCompatActivity implements AdapterView
     private Location location;
     private String category;
 
+    private static Location emptyLocation = new Location("");
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_search);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_item_search);
 
-        locationSpinner = (Spinner) findViewById(R.id.itemSearchLocationSpinner);
-        categorySpinner = (Spinner) findViewById(R.id.itemSearchCategorySpinner);
-        locationSpinner.setOnItemSelectedListener(this);
-        categorySpinner.setOnItemSelectedListener(this);
-        nameText = (EditText) findViewById(R.id.editText);
+            locationSpinner = (Spinner) findViewById(R.id.itemSearchLocationSpinner);
+            categorySpinner = (Spinner) findViewById(R.id.itemSearchCategorySpinner);
+            locationSpinner.setOnItemSelectedListener(this);
+            categorySpinner.setOnItemSelectedListener(this);
+            nameText = (EditText) findViewById(R.id.editText);
 
-        Log.d("Donatrix", "Hello");
-        resultSpinner = (Spinner) findViewById(R.id.itemSearchResultSpinner);
-        resultSpinner.setOnItemSelectedListener(this);
-        List<Location> locList = LocationDao.getLocations(this);
-        locList.add(0, null);
-        ArrayAdapter<Location> locationArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, locList.toArray());
-        locationArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        locationSpinner.setAdapter(locationArrayAdapter);
+            Log.d("Donatrix", "Hello");
+            resultSpinner = (Spinner) findViewById(R.id.itemSearchResultSpinner);
+            resultSpinner.setOnItemSelectedListener(this);
+            List<Location> locList = LocationDao.getLocations(this);
+            locList.add(0, emptyLocation);
+            ArrayAdapter<Location> locationArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, locList.toArray());
+            locationArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            locationSpinner.setAdapter(locationArrayAdapter);
 
-        Log.d("Donatrix", "Hello2");
-        ItemCategory[] itemCategoryArray = ItemCategory.values();
-        List<String> categories = new ArrayList<>();
-        for (ItemCategory i: itemCategoryArray) {
-            categories.add(i.getCategory());
+            Log.d("Donatrix", "Hello2");
+            ItemCategory[] itemCategoryArray = ItemCategory.values();
+            List<String> categories = new ArrayList<>();
+            for (ItemCategory i : itemCategoryArray) {
+                categories.add(i.getCategory());
+            }
+            categories.add(0, "");
+            ArrayAdapter<String> itemCategoryArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories.toArray());
+            Log.d("Donatrix", "Hello3");
+            itemCategoryArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            Log.d("Donatrix", "Hello5");
+            categorySpinner.setAdapter(itemCategoryArrayAdapter);
+            Log.d("Donatrix", "Hello4");
+        } catch (Exception e) {
+            Log.d("Donatrix", "Something is very wrong", e);
         }
-        categories.add(0, "");
-        ArrayAdapter<String> itemCategoryArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, categories.toArray());
-        Log.d("Donatrix", "Hello3");
-        itemCategoryArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Log.d("Donatrix", "Hello5");
-        categorySpinner.setAdapter(itemCategoryArrayAdapter);
-        Log.d("Donatrix", "Hello4");
     }
 
     @Override
@@ -81,7 +87,7 @@ public class ItemSearchActivity extends AppCompatActivity implements AdapterView
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
         category = "";
-        location = null;
+        location = emptyLocation;
     }
 
     public void onCancelButtonPressed(View view) {
@@ -90,7 +96,7 @@ public class ItemSearchActivity extends AppCompatActivity implements AdapterView
 
     public void onSearchButtonPressed(View view) {
         Map<String, Object> body = new HashMap<>();
-        if (location != null) {
+        if (location.getName().length() > 0) {
             body.put("loc_id", location.getKey());
         }
         if (category.length() > 0) {
