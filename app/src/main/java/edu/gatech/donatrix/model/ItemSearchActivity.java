50d360931439cecdb76d5cc61a30b15e76ba3f32
zday123 <zday123@yahoo.com>
2018-11-06 12:56:05 -0500

    Trying to finish m8. Pushing ItemSearchActivity

---
+++ app/src/main/java/edu/gatech/donatrix/model/ItemSearchActivity.java
@@ -0,0 +1,69 @@
+package edu.gatech.donatrix.model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import edu.gatech.donatrix.R;
+import edu.gatech.donatrix.dao.Database;
+import edu.gatech.donatrix.dao.LocationDao;
+
+public class ItemSearchActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner locationSpinner;
+    private Spinner categorySpinner;
+
+    private Location location;
+    private ItemCategory category;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_search);
+
+        locationSpinner = (Spinner) findViewById(R.id.itemSearchLocationSpinner);
+        categorySpinner = (Spinner) findViewById(R.id.itemSearchCategorySpinner);
+        locationSpinner.setOnItemSelectedListener(this);
+        categorySpinner.setOnItemSelectedListener(this);
+
+        List<Location> locList = LocationDao.getLocations(this);
+        locList.add(0, null);
+        ArrayAdapter<Location> locationArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, locList.toArray());
+        locationArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationSpinner.setAdapter(locationArrayAdapter);
+
+        ItemCategory[] itemCategoryArray = ItemCategory.values();
+        List<ItemCategory> itemCategories = Arrays.asList(itemCategoryArray);
+        itemCategories.add(0, null);
+        ArrayAdapter<ItemCategory> itemCategoryArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, itemCategories.toArray());
+        itemCategoryArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(itemCategoryArrayAdapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Spinner spinner = (Spinner) parent;
+        if (spinner.getId() == R.id.itemSearchLocationSpinner) {
+            location = (Location) spinner.getItemAtPosition(position);
+        } else if (spinner.getId() == R.id.itemSearchCategorySpinner) {
+            category = (ItemCategory) spinner.getItemAtPosition(position);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        category = null;
+        location = null;
+    }
+
+    public void onCancelButtonPressed(View view) {
+        finish();
+    }
+}
